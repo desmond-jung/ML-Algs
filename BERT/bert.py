5c52c1b42c1fa8a6ce74ad1db4e6d5d49b733cc6
Desmond Jung <desmond.jung@oracle.com>
2025-05-03 00:30:53 -0700

    Adding BERT folder

---
+++ BERT/bert.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+import torch
+import re
+import random
+import transformers, datasets
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertTokenizer
+import tqdm
+from torch.utils.data import Dataset, DataLoader
+import itertools
+import math
+import torch.nn.functional as F
+import numpy as np
+from torch.optim import Adam
